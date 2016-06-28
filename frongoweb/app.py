76c6ce8857abad3c502ff1710d9ea6f296a8f2c2
Marc Hanheide <marc@hanheide.de>
2016-06-28 11:38:14 +0100

    updated with first charts

--- frongoweb/app.py
+++ frongoweb/app.py
@@ -52,14 +52,53 @@ class Index:
     def GET(self):
         data = {
           'submit_url': '/query',
-          'queries': ['test1', 'test2']}
+          'queries': ['test1', 'test2']
+        }
         return renderer.index(data)
 
 
 class Query:
 
-    def POST(self):
-        return web.ok()
+    def prepare_plot(self):
+        probs_data = [.1, .2, .5, .7, .3, .6]
+        ent_data = [.3, .1, .2, .5, .7, .3]
+        dataset_probs = {
+            'label': 'Probability',
+            'fillColor': "rgba(0,0,220,0.3)",
+            'strokeColor': "rgba(0,0,220,1)",
+            'pointColor': "rgba(0,0,220,1)",
+            'pointStrokeColor': "#fff",
+            'pointHighlightFill': "#fff",
+            'pointHighlightStroke': "rgba(220,220,220,1)",
+            'data': probs_data
+        }
+
+        dataset_ent = {
+            'label': 'Entropy',
+            'fillColor': "rgba(0,220,120,0.3)",
+            'strokeColor': "rgba(0,220,120,1)",
+            'pointColor': "rgba(0,220,120,1)",
+            'pointStrokeColor': "#fff",
+            'pointHighlightFill': "#fff",
+            'pointHighlightStroke': "rgba(220,220,220,1)",
+            'data': ent_data
+        }
+
+        data = {
+            'labels': [str(s) for s in probs_data],
+            'datasets': [dataset_probs, dataset_ent]
+        }
+
+        # dataset = {
+        #     'label': "responses",
+        #     'fillColor': "rgba(120,0,0,0.5)",
+        #     'data': [results[r] for r in sorted_keys]
+        # }
+
+        return dumps(data, default=json_util.default)
+
+    def GET(self):
+        return self.prepare_plot()
 
 
 def signal_handler(signum, frame):
