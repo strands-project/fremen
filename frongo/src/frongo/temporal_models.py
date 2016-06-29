7a6df21d0d0d4d43b4062c89b2e7d1982f23b07b
Jaime Pulido Fentanes <Jailander@users.noreply.github.com>
2016-06-29 20:15:25 +0100

    Adding get_info service and test launch file (#31)
    * returning epoch in request prediction service
    
    * Create README.md
    
    * Now fremeniser can start without yaml file, yaml defs can be loaded through the service `/frongo/add_model_defs /frongo/add_model_defs` and script for testing this is added
    
    * Update README.md
    
    * Update README.md
    
    * Adding predict state order service
    
    * [Frongo] Now you can configure linear normalisation for float type models
    
    * Update README.md
    
    * Adding get_info service and test launch file

--- frongo/src/frongo/temporal_models.py
+++ frongo/src/frongo/temporal_models.py
@@ -1,4 +1,5 @@
 import json
+import yaml
 from frongo.fremen_interface import *
 
 def get_field(item, key):
@@ -89,6 +90,25 @@ class TModels(object):
             order= self.order
         probs=self._fremen.predict_outcome(epochs, self.name, order)
         return probs
+
+    def _get_info(self):
+        a = dir(self)
+        b =[]
+        s = {}
+        for i in a:
+            if not i.startswith('_'):
+                b.append(str(i))
+      
+        for i in b:
+            if type(self.__getattribute__(i)) is not list:
+                s[str(i)] =  self.__getattribute__(i)
+            else:
+                st= '[list with ' + str(len(self.__getattribute__(i))) +' ' + str(type(self.__getattribute__(i)[0])) + ' elements]'
+                s[str(i)] = st
+        
+        out=yaml.safe_dump(s,default_flow_style=False)
+        return out
+
 
     def __repr__(self):
         a = dir(self)
